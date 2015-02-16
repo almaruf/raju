e47d12cb55742be454ab0240ce84a5281df52945
Md Abdullah Al Maruf <maruf.sylhet@gmail.com>
2015-02-16 16:30:48 +0000

    see the difference
    Keep code compact , its good to see

--- blood-donation/admin/donor_g1.php
+++ blood-donation/admin/donor_g1.php
@@ -1,59 +1,41 @@
 <html>
-		<head>
-		<title>admin login</title>
-	
-		<link rel="stylesheet" type="text/css" href="include/style.css"/>
+    <head>
+    <title>admin login</title>
+    <link rel="stylesheet" type="text/css" href="include/style.css"/>
+    </head>
+<body>
+    <div class="wrapper">
+        <?php include_once('include/header.php'); ?>		
+        
+    <div class="content_area">
+        <div class="redline"></div>
 
-		</head>
-	<body>
-		<div class="wrapper">
-			<?php
-				include_once('include/header.php');
-			?>		
-		<div class="content_area">
-			<div class="redline">
-			</div>
-
-			<div class="left_content">
-		
-		
-				<div class="para">
-						<p>Home</p>
-						<p>Reports</p>
-					
-				<div class="list">
-					
-					<ul>
-						<li>Donor's overall report</li>
-						<li>Donor's Organization</li>
-						<li>Donor's overall report</li>
-					</ul>
-											
-					
-				</div>
-				
-				<div class="box">
-						<p><font color="#7D2420" face="times new roman">You Have added </font></p><br />
-						<p> <font color="red" face="times new roman">333</font><font color="#7D2420" face="times new roman">Donors</font></p><br />
-						<p> <font color="#7D2420" face="times new roman">You Have managed </font></p><br />
-						<p> <font color="red" face="times new roman">333</font> <font color="#7D2420" face="times new roman">Donors</font> </p><br />
-						<p> <font color="#7D2420" face="times new roman">You have Donated</font></p><br />
-						
-						<p> <font color="red" face="times new roman">5</font><font color="#7D2420" face="times new roman">times this year</font> </p><br />
-				</div>
-	</div>
-		</div>
-		 
-		 
-		 	
-		</div>	
-		<div class="redline">
-		 
-		 </div>	
-			<?php
-				include_once('include/footer.php');			
-			?>
-	</div>
+        <div class="left_content">
+            <div class="para">
+                <p>Home</p>
+                <p>Reports</p>
+            <div class="list">
+                <ul>
+                    <li>Donor's overall report</li>
+                    <li>Donor's Organization</li>
+                    <li>Donor's overall report</li>
+                </ul>
+            </div>
+            
+            <div class="box">
+                <p><font color="#7D2420" face="times new roman">You Have added </font></p><br />
+                <p> <font color="red" face="times new roman">333</font><font color="#7D2420" face="times new roman">Donors</font></p><br />
+                <p> <font color="#7D2420" face="times new roman">You Have managed </font></p><br />
+                <p> <font color="red" face="times new roman">333</font> <font color="#7D2420" face="times new roman">Donors</font> </p><br />
+                <p> <font color="#7D2420" face="times new roman">You have Donated</font></p><br />
+                <p> <font color="red" face="times new roman">5</font><font color="#7D2420" face="times new roman">times this year</font> </p><br />
+            </div>
+        </div>
+    </div>
+    </div>	
+    <div class="redline"></div>	
+    <?php include_once('include/footer.php'); ?>
+</div>
 </body>
 </html>			
-		
+    
