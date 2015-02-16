e9a800661a6db31467370675e80d05300b816f21
Rajumeazi <rajumeazi@gmail.com>
2015-02-16 22:15:15 +0600

    new file:   blood-donation/admin/donor_details.php 	new file:   blood-donation/admin/donor_details.php~ 	new file:   blood-donation/admin/donor_g1.php 	new file:   blood-donation/admin/donor_g2.php 	new file:   blood-donation/admin/donor_query.php 	new file:   blood-donation/admin/images/check0.png 	new file:   blood-donation/admin/include/check0.png 	new file:   blood-donation/admin/include/footer.php 	new file:   blood-donation/admin/include/footer.php~ 	new file:   blood-donation/admin/include/header.php 	new file:   blood-donation/admin/include/header.php~ 	new file:   blood-donation/admin/include/mystyle.css 	new file:   blood-donation/admin/include/reset.css 	new file:   blood-donation/admin/include/reset.css~ 	new file:   blood-donation/admin/include/style.css 	new file:   blood-donation/admin/include/style.css~ 	new file:   blood-donation/admin/include/table.css 	new file:   blood-donation/admin/include/table.css~ 	new file:   blood-donation/admin/index.php 	new file:   blood-donation/admin/index.php~

---
+++ blood-donation/admin/include/header.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+					
+			<div class="header">
+				<p class="headline">Directory for blood donor's of Bangladesh</p>		
+				
+			<div class="nav1">
+				<div id="help"> <a href="###">Contact Dev</a></div> 				
+				<a  href="###">Help |</a> 			
+			<div id="state">			<a href="###">Logout |</a></div>
+			
+				
+			</div>
+		 
+		 </div>	
+	</body>
+</html>
+		
+	
